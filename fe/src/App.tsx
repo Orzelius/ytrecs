3e63031d0ea9bd880d8d139c0212d049787ada4e
Orzelius <albertkostusev@gmail.com>
2024-03-01 20:48:46 +0900

    change folder structure

---
+++ fe/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <script src="http://www.gmodules.com/ig/ifr?url=http://www.google.com/ig/modules/youtube.xml&up_channel=Orzelius&synd=open&w=320&h=390&title=&border=%23ffffff%7C3px%2C1px+solid+%23999999&output=js"></script>
+    </div>
+  );
+}
+
+export default App;
